292766f3d20e4b89defd69db2a1f0907d3c43a54
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-29 16:04:03 +0100

    setup route, event handlers and listener

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,4 +6,28 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+// Routes and event handlers
+app.post('/events', (req, res) => {
+  const event = req.body;
 
+  // send event to:
+  // Post microservice
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  // Comment microservice
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  // Query microservice
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err.message);
+  });
+
+  res.send({status: 'OK'});
+})
+
+// Event bus listener
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+})
