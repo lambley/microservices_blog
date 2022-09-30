9b54fad72bf86135e57efd9aa7d4f7f1e765b88f
Aaron Lambley <aaron.lambley@gmail.com>
2022-09-30 15:18:44 +0100

    update to reflect comment moderation status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,24 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = Array.isArray(comments) ? comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    // check moderation status and display respective content
+    switch(comment.status) {
+      case 'approved':
+        content = comment.content;
+        break;
+      case 'pending':
+        content = 'This comment is awaiting moderation'
+        break;
+      case 'rejected':
+        content = 'This comment has been rejected'
+        break;
+      default:
+        break;
+    }
+
+    return <li key={comment.id}>{content}</li>;
   }) : null;
 
   return <ul>{renderedComments}</ul>;
